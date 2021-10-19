f93a6d8879caab2fc2e36c2a1a9b6273d6232b44
Andrew Qolta <andrew.azmy13@gmail.com>
2021-10-18 23:10:50 -0500

    clean code

--- Calculator.API/Startup.cs
+++ Calculator.API/Startup.cs
@@ -2,17 +2,10 @@ using Calculator.Domain;
 using Calculator.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Calculator.API
 {
@@ -28,12 +21,9 @@ namespace Calculator.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-
             services.AddSingleton<IDatabaseConnectionString>((serviceProvider) => new DatabaseConnectionString(Configuration.GetConnectionString("SqlConn")));
             services.AddTransient<IUnitOfWork, UnitOfWork>();
-
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Calculator.API", Version = "v1" });
